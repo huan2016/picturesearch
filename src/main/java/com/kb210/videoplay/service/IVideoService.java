312d647d7db0bb3f1fc8cc0c41eeef3000ffac5b
victoryyuxing <1598803521@qq.com>
2016-09-03 20:06:57 +0800

    init

--- src/main/java/com/kb210/videoplay/service/IVideoService.java
+++ src/main/java/com/kb210/videoplay/service/IVideoService.java
@@ -1,9 +1,5 @@
 package com.kb210.videoplay.service;
 
-import java.util.List;
-
-import com.kb210.videoplay.entity.Video;
-
 public interface IVideoService {
 
 }
--- src/main/java/com/kb210/videoplay/service/impl/VideoServiceImpl.java
+++ src/main/java/com/kb210/videoplay/service/impl/VideoServiceImpl.java
@@ -1,12 +1,5 @@
 package com.kb210.videoplay.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.kb210.videoplay.entity.Video;
-import com.kb210.videoplay.mapper.VideoMapper;
 import com.kb210.videoplay.service.IVideoService;
 
 //@Service
